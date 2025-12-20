44db6acf10938ab10a1eb95902834fef18241dbf
Manish Khanal <112839147+Manish-Khanalv1@users.noreply.github.com>
2025-12-19 23:28:32 -0700

    added a power output plot, new files from the pole, new images, lim set, removed the debug script

--- single_day_plot.py
+++ single_day_plot.py
@@ -32,13 +32,13 @@ def date2num(date_str): # Renamed 'date' to 'date_str' to avoid confusion with t
     # 29 # return the day of the year (%j) as an integer
     # The strftime method returns a string, so we convert it to an integer.
     return int(date_object.strftime('%j'))
-print(f'DEBUG: {input_2}')
+# print(f'DEBUG: {input_2}')
 date = file_101['Date']
 NumberDate = date.apply(date2num)
 mask_to_keep =  NumberDate <= int(input_2)
 file_101 = file_101[mask_to_keep]
-print(f'DEBUG: {mask_to_keep}')
-print(f'DEBUG: {NumberDate}')
+# print(f'DEBUG: {mask_to_keep}')
+# print(f'DEBUG: {NumberDate}')
 
 date = file_103['Date']
 NumberDate = date.apply(date2num)
@@ -54,7 +54,10 @@ current_101 = file_101['A']
 time_103 = file_103['Time']
 voltage_103 = file_103['V'] 
 current_103 = file_103['A'] 
-
+print(f"Average Power 101: {np.mean(voltage_101*current_101)} W")
+print(f"Average Power 103: {np.mean(voltage_103*current_103)} W")
+print(f"Max Power 101: {np.max(voltage_101*current_101)} W, min: {np.min(voltage_101*current_101)} W")
+print(f"Max Power 103: {np.max(voltage_103*current_103)} W, min: {np.min(voltage_103*current_103)} W")
 #Create a figure and axis objects
 kwargs_101 = {'marker' : 'o', 's' : 2}
 kwargs_103 = {'marker' : '^', 's' : 2}
@@ -63,8 +66,8 @@ plt.scatter(time_101, voltage_101, label='Device 101 Voltage (V)', color='blue',
 plt.scatter(time_101, current_101, label='Device 101 Current (A)', color='red', **kwargs_101)
 plt.scatter(time_103, voltage_103, label='Device 103 Voltage (V)', color='green', **kwargs_103)
 plt.scatter(time_103, current_103, label='Device 103 Current (A)', color='orange', **kwargs_103)
-print(f"DEBUG: Length of timestamp 101: {len(time_101)}")
-print(f"DEBUG: Length of timestamp 103: {len(time_103)}")
+# print(f"DEBUG: Length of timestamp 101: {len(time_101)}")
+# print(f"DEBUG: Length of timestamp 103: {len(time_103)}")
 plt.title(f'Panels Voltage and Current on Day {input_2} of 20{input_1}')
 plt.xlabel('Time of the Day')
 plt.ylabel('Voltage (V) and Current (A)')
@@ -77,6 +80,28 @@ ax.set_xticks(np.linspace(0, int(end), 17).astype(int))
 #rotate the x axis labels by 45 degrees
 plt.xticks(rotation=45)
 plt.savefig(f"{OutputPath}/spo_dev101_103_{input_1}_{input_2}_plot.png", bbox_inches='tight', dpi=100,facecolor='white')
+plt.show()
+
+plt.figure(figsize=(12, 6))
+plt.scatter(time_101, voltage_101*current_101, label='Device 101 Power', color='blue', **kwargs_101)
+plt.scatter(time_103, voltage_103*current_103, label='Device 103 Voltage (V)', color='green', **kwargs_103)
+# print(f"DEBUG: Length of timestamp 101: {len(time_101)}")
+# print(f"DEBUG: Length of timestamp 103: {len(time_103)}")
+plt.title(f'Panels Power on Day {input_2} of 20{input_1}')
+plt.xlabel('Time of the Day')
+plt.ylabel('Power [W]')
+plt.legend(loc = 'best')
+plt.ylim(0,480)
+#y ticks every 50 W
+plt.yticks(np.arange(0, 481, 50))
+plt.grid(True)
+ax = plt.gca()
+start, end = ax.get_xlim() # Get the range of the x-axis (e.g., 0 to N)
+# Select 15 evenly spaced integers across the range
+ax.set_xticks(np.linspace(0, int(end), 17).astype(int))
+#rotate the x axis labels by 45 degrees
+plt.xticks(rotation=45)
+plt.savefig(f"{OutputPath}/power_spo_dev101_103_{input_1}_{input_2}_plot.png", bbox_inches='tight', dpi=100,facecolor='white')
 plt.show()
 
 
