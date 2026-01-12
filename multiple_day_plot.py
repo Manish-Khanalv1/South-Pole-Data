534a57ab6df7d697ab58124283687f329370a0e3
Griffie Liedtke <gliedtke@cobalt-15.icecube.wisc.edu>
2026-01-12 17:41:43 -0600

    Started Analysing multi day plots but the code is not yet working

---
+++ multiple_day_plot.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.dates as mdates
+import os
+import urllib.request
+import random
+
+
+def date2num(date_str): # Renamed 'date' to 'date_str' to avoid confusion with the datetime.date object
+    # 26 # first conver the xxxx-xx-xx format to date(xxxx, xx, xx)
+
+    # You can skip splitting the string and go directly to parsing:
+    date_object = datetime.strptime(date_str, '%Y-%m-%d').date()
+    
+    # 29 # return the day of the year (%j) as an integer
+    # The strftime method returns a string, so we convert it to an integer.
+    return int(date_object.strftime('%j'))
+
+
+year = input('Enter last 2 digits of the year: ')
+StartDay = int(input('                   Starting Day: '))
+EndDay = int(input('                     Ending Day: '))
+
+DayRange =  EndDay - StartDay
+
+Days_101 = []
+Days_103 = []
+for i in range(DayRange):
+    file_101 = pd.read_csv(f"DataFolder/spo_dev101_{year}_{i + StartDay}.csv")
+    file_103 = pd.read_csv(f"DataFolder/spo_dev103_{year}_{i + StartDay}.csv")
+
+    file_101.rename(columns = {'Device ID': 'V', 'Voltage': 'A'}, inplace=True)
+    file_103.rename(columns = {'Device ID': 'V', 'Voltage': 'A'}, inplace=True)
+
+    date = file_101['Date']
+    NumberDate = date.apply(date2num)
+    mask_to_keep =  NumberDate <= int(i)
+    file_101 = file_101[mask_to_keep]
+    # print(f'DEBUG: {mask_to_keep}')
+    # print(f'DEBUG: {NumberDate}')
+    
+    date = file_103['Date']
+    NumberDate = date.apply(date2num)
+    mask_to_keep = NumberDate <= int(i)
+    file_103 = file_103[mask_to_keep]
+    
+    #Extract time, voltage and current data
+    
+    time_101 = file_101['Time']
+    voltage_101 = file_101['V'] 
+    current_101 = file_101['A'] 
+    
+    time_103 = file_103['Time']
+    voltage_103 = file_103['V'] 
+    current_103 = file_103['A'] 
+    
+    power_101 = voltage_101 * current_101
+    power_103 = voltage_103 * current_103
+    
+    Device_101_Dict = {'time': time_101, 'voltage': voltage_101, 'current': current_101, 'power': power_101}
+    Device_103_Dict = {'time': time_103, 'voltage': voltage_103, 'current': current_103, 'power': power_103}
+
+    Days_101.append(Device_101_Dict)
+    Days_103.append(Device_103_Dict)
+
+
+plt.figure()
+for i in range(DayRange):
+
+    PowerData = Device_101_dict[i]['power']
+    TimeData = Device_101_dict[i]['time']
+        
+    plt.plot(TimeData, PowerData)
+plt.savefig(f"MultiDayPlotsFolder/MultipledayPlots")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
